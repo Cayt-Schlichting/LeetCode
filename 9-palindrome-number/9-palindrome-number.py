ce8272d2f28890439da6bd344d7d85b15d063c98
Cayt Schlichting <102182096+Cayt-Schlichting@users.noreply.github.com>
2022-09-28 12:32:57 -0700

    Time: 164 ms (8.34%), Space: 14 MB (16.10%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.py
@@ -0,0 +1,20 @@
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        #vars to use
+        i=0
+        #convert number to list of digits
+        l_x = list(str(x))
+        while i < len(l_x) // 2:
+            #Get second index spot
+            i2 = -1 - i
+            #Compare two index spots
+            if l_x[i] == l_x[i2]: 
+                # increment i and continue through loop
+                i +=1
+                continue
+            else:
+                return False
+        #If we made it through the whole loop w/o exiting the function, it's a palindrome!
+        return True
+            
+        
